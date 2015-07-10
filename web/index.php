4f79b95ad2d8fe2714d87dc8e1a71f8cd7ad17d5
unknown <mcasalino@NCEL16491.iis.amadeus.net>
2015-07-10 16:12:29 +0200

    First webapp

---
+++ web/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+$dsn = 'pgsql:'
+    . 'host='.$dbopts["host"].';'
+    . 'dbname='.ltrim($dbopts["path"],'/').';'
+    . 'user='.$dbopts["user"].';'
+    . 'port='.$dbopts["port"].';'
+    . 'sslmode=require;'
+    . 'password='.$dbopts["pass"];
+try
+{
+	$db = new PDO($dsn);
+}
+catch(PDOException $pe)
+{
+	die('Connection error, because: ' .$pe->getMessage());
+}
+
+?>
+<html>
+<head></head>
+<body><h1><?php echo "hello world from php"; ?></h1></body>
+</html>
+
+<?php
+
+$db=null;
+
+?>
