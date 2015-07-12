4e0d84b68b404dfbabb7eecfbce8e1f8b315b93c
Matteo Casalino <matteo.casalino@gmail.com>
2015-07-12 22:12:12 +0200

    go to real db

--- web/resultsAjax.php
+++ web/resultsAjax.php
@@ -2,18 +2,32 @@
 
 <?php
 
-$result = [
+$sql = 	"select p.name, p.description, p.picture, count(*) "
+	. "from profile p, offered_service s "
+	. "where p.id = s.profile_id "
+	. "and s.available = TRUE ";
+	
+$services = preg_grep_keys("/service__\d+/",$_POST);
+if (count($services)>0) {
+	$sql .= "and s.service_type in (".implode($services,', ').")";
+}
+	
+$sql .=	"group by p.id, p.name, p.description, p.picture "
+	. "order by count(*) desc";
 
-[
-	'name' => 'Dupont',
-	'description' => random_lipsum(),
-	'picture' => 'img/profiles/dupont.jpeg',
-	'stars' => '4',
-	'services' => [ServiceType::Stroller, ServiceType::CarSeat],
-	'_POST' => $_POST
-]
+$q = $db->prepare($sql);
+$q->execute();
 
-];
+$result = [];
+
+while ($row = $q->fetch(PDO::FETCH_ASSOC)) {
+	$result[] = array_merge($row,
+	[	
+		'stars' => '4',
+		'services' => [ServiceType::Stroller, ServiceType::CarSeat],
+		//'_POST' => $_POST
+	]);
+}
 
 echo json_encode($result);
 ?>
