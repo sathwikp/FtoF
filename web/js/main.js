f8b5f93c66031fdc07b1a051b933046100a186b3
Matteo Casalino <matteo.casalino@gmail.com>
2015-07-14 22:49:13 +0200

    baby counters decrease buttons

--- web/js/main.js
+++ web/js/main.js
@@ -93,9 +93,22 @@ $(document).ready(function(){
 	});
 	
 	$('.round-button').click(function(){
-		var theCurrVal=Number($(this).next('input').val());
+		var theCurrVal=Number($(this).siblings('input').val());
 		var theNewVal = theCurrVal+1;
-		$(this).next('input').val(theNewVal);
-		$(this).find('span > span').text(theNewVal);
+		$(this).siblings('input').val(theNewVal);
+		$(this).find('span > span.counter').text(theNewVal);
 	});
+	
+	$('.decrease').click(function(e){
+		var theCurrVal=Number($(this).closest('.round-button').siblings('input').val());
+		if (theCurrVal > 0) {
+			var theNewVal = theCurrVal-1;
+			$(this).closest('.round-button').siblings('input').val(theNewVal);
+			$(this).parent().find('span.counter').text(theNewVal);
+		}
+		e.preventDefault();
+		return false;
+	});
+	
+	  $('[data-toggle="tooltip"]').tooltip();
 });
