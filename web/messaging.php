4a0aa6f1695092d53b4362fafb102747781024ff
Matteo Casalino <matteo.casalino@gmail.com>
2015-07-15 14:56:56 +0200

    messaging test

--- web/messaging.php
+++ web/messaging.php
@@ -1,17 +1,25 @@
 <?php require 'init.php.inc'; ?>
+
+<?php require 'vendor/autoload.php'; ?>
+
 <pre>
+
 <?php
-
-require 'vendor/autoload.php';
-
+echo "instantiating SendGrid \n";
 $sendgrid = new SendGrid($api_user, $api_key);
+
+echo "instantiating Email \n";
 $email    = new SendGrid\Email();
 
+
+echo "instantiating message \n";
 $message->addTo('daria.dubin@googlemail.com')->
-          setFrom('some@email.com')->
+          setFrom('daria.dubin@googlemail.com')->
           setSubject('My test message')->
           setText('Hello World!')->
           setHtml('<strong>Hello World!</strong>');
+
+echo "Sending message \n";          
 $response = $sendgrid->send($message);
 
 print_r($response);
